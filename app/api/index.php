ae89096eaeda2f81f916346906f1295d105d55ce
ic-434 <akbalinartu@mail.ru>
2023-02-22 14:12:53 +0500

    php and main_js

---
+++ app/api/index.php
@@ -0,0 +1,10 @@
+<?php
+//$htmlfiles = glob("../../*.html");
+//$response = [];
+
+//foreach ($htmlfiles as $file) {
+//    array_push($response, basename($file));
+//}
+
+echo "hell_______!";
+//echo join_encode($response);
--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,10 @@
+//import jQuery, { data } from "jquery";
 import jQuery from "jquery";
 
-jQuery("h1").fadeOut()
+jQuery.get("./api", data => {
+  //  data.forEach(File => {
+        console.log(data);
+        //jQuery("body").append(`<h1>jQuery{file}</h1>`)
+    })
+//}, "JSON");
 
